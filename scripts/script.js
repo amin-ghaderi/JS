2bc435b403b3105a227cddc264e48a6041d4c710
Amin Ghaderi <agh.ghaderi@gmail.com>
2026-05-11 10:31:34 +0200

    Add temperature conversion functions

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,17 @@ function filterEvenNumbers(numbers) {
 let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
 console.log(filterEvenNumbers(myNumbers));
+
+function celsiusToFahrenheit(celsius) {
+
+    return (celsius * 9 / 5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+console.log("50°C is " + celsiusToFahrenheit(50) + "°F");
+
+console.log("78°F is " + fahrenheitToCelsius(78).toFixed(3) + "°C");
