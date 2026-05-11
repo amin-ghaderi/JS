c358d63118efe3249a6112451a47045b7076f3e7
Amin Ghaderi <agh.ghaderi@gmail.com>
2026-05-11 10:28:35 +0200

    Implement even number filtering function

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-console.log("JS connected");
+function filterEvenNumbers(numbers) {
+
+    let result = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+
+        if (numbers[i] % 2 !== 0) {
+            result.push(numbers[i]);
+        }
+    }
+
+    return result;
+}
+
+let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+console.log(filterEvenNumbers(myNumbers));
