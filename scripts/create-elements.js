cc07a4c4f032ad8c0d5ca1e6bd40eb646c51a55e
Amin Ghaderi <agh.ghaderi@gmail.com>
2026-05-12 12:22:34 +0200

    Add image formats array for create elements

---
+++ scripts/create-elements.js
@@ -0,0 +1,6 @@
+const imageFormats = [
+    "AVIF is an AV1 bytestream in a HEIF container.",
+    "WebP is a VP8 bytestream (or lossless WebP) in a RIFF container.",
+    "Sometimes, an image format defines its own container.",
+    "PNG, JPEG, and JPEG XL include the layout as part of the file format specification."
+];
